73a165ac9249db80341ed806ce950fe932e002f3
mcstitzer <mcstitzer@ucdavis.edu>
2022-07-07 10:48:15 -0700

    Create get_updated_te_gffs.sh

---
+++ genomes_and_annotations/tes/get_updated_te_gffs.sh
@@ -0,0 +1,8 @@
+mkdir -p new_edta_tes
+cd new_edta_tes
+
+#wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B73.PLATINUM.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/
+
+## I'm not sure of the difference between split and regular files... check once downloaded!
