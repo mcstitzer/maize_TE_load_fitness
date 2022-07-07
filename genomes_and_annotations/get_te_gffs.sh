316eaffaf3b8fdc9ea16c00c525f3be307eccf22
mcstitzer <mcstitzer@ucdavis.edu>
2022-07-07 10:37:05 -0700

    Create get_te_gffs.sh

---
+++ genomes_and_annotations/get_te_gffs.sh
@@ -0,0 +1,31 @@
+
+## Download and unzip all the EDTA TE annotations from maizegdb. There's probably a better way to do this!
+
+wget https://download.maizegdb.org/Zm-B73-REFERENCE-NAM-5.0/Zm-B73-REFERENCE-NAM-5.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-B97-REFERENCE-NAM-1.0/Zm-B97-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML52-REFERENCE-NAM-1.0/Zm-CML52-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML69-REFERENCE-NAM-1.0/Zm-CML69-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML103-REFERENCE-NAM-1.0/Zm-CML103-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML228-REFERENCE-NAM-1.0/Zm-CML228-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML247-REFERENCE-NAM-1.0/Zm-CML247-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML277-REFERENCE-NAM-1.0/Zm-CML277-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML322-REFERENCE-NAM-1.0/Zm-CML322-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-CML333-REFERENCE-NAM-1.0/Zm-CML333-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-HP301-REFERENCE-NAM-1.0/Zm-HP301-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Il14H-REFERENCE-NAM-1.0/Zm-Il14H-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Ki3-REFERENCE-NAM-1.0/Zm-Ki3-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Ki11-REFERENCE-NAM-1.0/Zm-Ki11-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Ky21-REFERENCE-NAM-1.0/Zm-Ky21-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-M37W-REFERENCE-NAM-1.0/Zm-M37W-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-M162W-REFERENCE-NAM-1.0/Zm-M162W-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Mo18W-REFERENCE-NAM-1.0/Zm-Mo18W-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Ms71-REFERENCE-NAM-1.0/Zm-Ms71-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-NC350-REFERENCE-NAM-1.0/Zm-NC350-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-NC358-REFERENCE-NAM-1.0/Zm-NC358-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Oh7B-REFERENCE-NAM-1.0/Zm-Oh7B-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Oh43-REFERENCE-NAM-1.0/Zm-Oh43-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-P39-REFERENCE-NAM-1.0/Zm-P39-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Tx303-REFERENCE-NAM-1.0/Zm-Tx303-REFERENCE-NAM-1.0.TE.gff3.gz
+wget https://download.maizegdb.org/Zm-Tzi8-REFERENCE-NAM-1.0/Zm-Tzi8-REFERENCE-NAM-1.0.TE.gff3.gz
+
+gunzip *.gz
