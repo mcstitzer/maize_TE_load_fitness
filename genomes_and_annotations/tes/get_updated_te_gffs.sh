8fec5c049235d379a0655c99db8347eb8bb7d0e6
mcstitzer <mcstitzer@ucdavis.edu>
2022-07-08 12:10:35 -0400

    Update get_updated_te_gffs.sh

--- genomes_and_annotations/tes/get_updated_te_gffs.sh
+++ genomes_and_annotations/tes/get_updated_te_gffs.sh
@@ -3,6 +3,61 @@ cd new_edta_tes
 
 #wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B73.PLATINUM.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
 
-wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/
+## :( doesn't work...
+#wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/
 
 ## I'm not sure of the difference between split and regular files... check once downloaded!
+
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B73.PLATINUM.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B73.PLATINUM.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B97.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/B97.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML103.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML103.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML228.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML228.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML247.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML247.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML277.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML277.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML322.pseudomolecules-v2.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML322.pseudomolecules-v2.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML333.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML333.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML52.pseudomolecules-v2.1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML52.pseudomolecules-v2.1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML69.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz 
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/CML69.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/HP301.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/HP301.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Il14H.pseudomolecules-v2.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Il14H.pseudomolecules-v2.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ki11.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ki11.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ki3.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ki3.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ky21.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ky21.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/M162W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/M162W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/M37W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/M37W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Mo18W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Mo18W.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ms71.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Ms71.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/NAM.EDTA2.0.0.MTEC02052020.TElib.fa
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/NC350.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/NC350.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/NC358.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/NC358.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Oh43.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Oh43.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Oh7b.pseudomolecules-v2.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Oh7b.pseudomolecules-v2.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/P39.pseudomolecules-v2.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/P39.pseudomolecules-v2.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Tx303.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Tx303.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Tzi8.pseudomolecules-v1.fasta.mod.EDTA.TEanno.gff3.gz
+wget --recursive --no-parent https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_Canu1.8_TE_annotation_03032022/Tzi8.pseudomolecules-v1.fasta.mod.EDTA.TEanno.split.gff3.gz
