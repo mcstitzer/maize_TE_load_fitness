c4615ca8ca0b7b4c4ae972f737ee1eaeeab1269f
mcstitzer <mcstitzer@ucdavis.edu>
2022-08-05 17:36:54 -0400

    Create ridge_regression.R

---
+++ models/ridge_regression.R
@@ -0,0 +1,19 @@
+library(stringr)
+library(dplyr)
+library(lme4)
+library(lme4qtl)
+library(ggplot2)
+library(cowplot)
+theme_set(theme_cowplot())
+library(plyr)
+
+
+teh=read.table('geno_pheno_gphenos.2022-08-05.txt', header=T)
+
+
+
+
+
+
+
+
