3e70a02156f65e5b58ab17d04e3df99eaa5cfa7c
mcstitzer <mcstitzer@ucdavis.edu>
2022-08-09 16:36:20 -0400

    Update hapids_to_taxa_matrices.eachTEfamily.R

--- imputation/hapids_to_taxa_matrices.eachTEfamily.R
+++ imputation/hapids_to_taxa_matrices.eachTEfamily.R
@@ -18,7 +18,7 @@ parents=read.table('phg_hapids_parents_dbout.txt', header=T)
 ## this is the values for each haplotype of how many TEs there are
 atorrm=read.table('allNAM_hapids.FamiliesUpdate.sup.2022-08-09.txt', header=T, comment.char='')
 
-
+## this part takes a long long time (2 hours??) - so it's good i've pulled it out from the below lapply :~)
 matchMatrix=data.frame(lapply(1:ncol(all.haps), function(refrange) match(all.haps[,refrange], atorrm$hapid)))
 colnames(matchMatrix)=colnames(all.haps)
 
@@ -47,14 +47,16 @@ write.table(rilsumKeep, paste0('ril_TEFam_bp_repeats.', Sys.Date(), '.txt'), row
 
 ### okay, i know this is horrible, but renaming parents to all.haps here because I don't want to rewrite all fo this
 all.haps=parents
-
-famMatrices=lapply(colnames(atorrm)[X:ncol(atorrm)], function(x){
- tempMat=sapply(1:ncol(all.haps), function(refrange) atorrm[,x][match(all.haps[,refrange], atorrm$hapid)])
+                                                          
+                                                          
+famMatrices=lapply(colnames(atorrm)[13:ncol(atorrm)], function(tefam){
+ tempMat=sapply(1:ncol(all.haps), function(refrange) atorrm[matchMatrix[,refrange],tefam])
  rownames(tempMat)=rownames(all.haps)
  colnames(tempMat)=colnames(all.haps)
  return(tempMat)
 })
-names(famMatrices)=colnames(atorrm)[X:ncol(atorrm)]
+names(famMatrices)=colnames(atorrm)[13:ncol(atorrm)]
+
 
 parentsumKeep=cbind(id=rownames(all.haps), data.frame(lapply(famMatrices, function(x) rowSums(x[,colnames(x) %in% paste0('X', b$refrange[b$KEEPfinalFilter])], na.rm=T)))
                     
