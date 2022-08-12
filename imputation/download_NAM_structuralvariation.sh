5b7adbe50d5d9388b5ce811f53f78da3998f7d92
mcstitzer <mcstitzer@ucdavis.edu>
2022-08-12 11:49:56 -0700

    Create download_NAM_structuralvariation.sh

---
+++ imputation/download_NAM_structuralvariation.sh
@@ -0,0 +1,31 @@
+
+
+mkdir -p NAM_SV
+
+cd NAM_SV
+
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-B97-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML103-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML228-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML247-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML277-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML322-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML333-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML52-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-CML69-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-HP301-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Il14H-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Ki11-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Ki3-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Ky21-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-M162W-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-M37W-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Mo18W-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Ms71-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-NC350-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-NC358-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Oh43-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Oh7B-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-P39-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Tx303-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
+wget https://de.cyverse.org/anon-files//iplant/home/shared/NAM/NAM_genome_and_annotation_Jan2021_release/STRUCTURAL_VARIATION/NAM_SVs_knobs_centromeres_on_B73_coordinates/Zm-Tzi8-REFERENCE-NAM-1.0_SV_knobs_centromeres_vs_B73_coordinates.bed
