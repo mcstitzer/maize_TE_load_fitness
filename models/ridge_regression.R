bcdb1036751864451adc259edde422175df3303a
mcstitzer <mcstitzer@ucdavis.edu>
2022-08-17 14:59:11 -0400

    Update ridge_regression.R

--- models/ridge_regression.R
+++ models/ridge_regression.R
@@ -135,14 +135,23 @@ error <- function(obs, pred){
 
 
 r=sample(1:nrow(tefocus), nrow(tefocus)*0.8)
-train=tefocus[r,] %>% na.omit()
-dim(train)
-test=tefocus[-r,] %>% na.omit()
-tefocusDTS=predictPhenotype(train_set=train[,-c(3:5)], test_set=test[,-c(3:5)], method='ridge', returnModel=T)
+train=tefocus[r,-c(3:5)] %>% na.omit()
+test=tefocus[-r,-c(3:5)] %>% na.omit()
+tefocusDTScor=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=F)
+tefocusDTS=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
 #cor.test(temp[,2], temp[,3])
-tefocusGY=predictPhenotype(train_set=train[,-c(2:3,5)], test_set=test[,-c(2:3,5)], method='ridge', returnModel=T)
-tefocusGYraw=predictPhenotype(train_set=train[,-c(2:4)], test_set=test[,-c(2:4)], method='ridge', returnModel=T)
+train=tefocus[r,-c(2:3,5)] %>% na.omit()
+test=tefocus[-r,-c(2:3,5)] %>% na.omit()
+tefocusGYcor=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=F)
+tefocusGY=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
+train=tefocus[r,-c(2:4)] %>% na.omit()
+test=tefocus[-r,-c(2:4)] %>% na.omit()
+tefocusGYrawcor=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=F)
+tefocusGYraw=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
 
+cor.test(tefocusDTScor[,2], tefocusDTScor[,3])
+cor.test(tefocusGYcor[,2], tefocusGYcor[,3])
+cor.test(tefocusGYrawcor[,2], tefocusGYrawcor[,3])
 
 b=broom::tidy(tefocusDTS)
 b=broom::tidy(tefocusGY)
@@ -150,113 +159,34 @@ b=broom::tidy(tefocusGYraw)
 
 tefocusRR=data.frame(term=broom::tidy(tefocusDTS)$term, tefocusDTS=broom::tidy(tefocusDTS)$estimate, tefocusGY=broom::tidy(tefocusGY)$estimate, tefocusGYraw=broom::tidy(tefocusGYraw)$estimate)
 
-## repeat focus
-
-r=sample(1:nrow(repeatfocus), nrow(repeatfocus)*0.8)
-train=repeatfocus[r,] %>% na.omit()
-dim(train)
-test=repeatfocus[-r,] %>% na.omit()
-trainDTS=train[,-c(3:5)]
-testDTS=test[,-c(3:5)]
-repeatfocusDTS=predictPhenotype(train_set=trainDTS, test_set=testDTS, method='ridge', returnModel=T)
-#cor.test(temp[,2], temp[,3])
-repeatfocusGY=predictPhenotype(train_set=train[,-c(2:3,5)], test_set=test[,-c(2:3,5)], method='ridge', returnModel=T)
-repeatfocusGYraw=predictPhenotype(train_set=train[,-c(2:4)], test_set=test[,-c(2:4)], method='ridge', returnModel=T)
 
 
-b=broom::tidy(repeatfocusDTS)
-b=broom::tidy(repeatfocusGY)
-b=broom::tidy(repeatfocusGYraw)
+mbTEs=read.table('../imputation/TE_content_across_NAM_genotypes_by_fam.2022-08-08.txt', header=T)
+mbTEs$Name=gsub('_LTR', '', gsub('_INT', '', mbTEs$Name))
+tefocusRR$Classification=mbTEs$Classification[match(tefocusRR$term, mbTEs$Name)]
+tefocusRR$totalbp=mbTEs$bp[match(tefocusRR$term, mbTEs$Name)]
 
-repeatfocusRR=data.frame(term=broom::tidy(repeatfocusDTS)$term, repeatfocusDTS=broom::tidy(repeatfocusDTS)$estimate, repeatfocusGY=broom::tidy(repeatfocusGY)$estimate, repeatfocusGYraw=broom::tidy(repeatfocusGYraw)$estimate)
+tefocusRR %>%group_by(Classification) %>% dplyr::summarize(posDTS=sum(tefocusDTS>0), negDTS=sum(tefocusDTS<0), posGY=sum(tefocusGY>0), negGY=sum(tefocusGY<0))
 
 
 
 
-## try with full matrix of each repeat class
+pdf('~/transfer/ridgeregression_tefams.pdf', 14,6)
+ggplot(tefocusRR[-1,], aes(x=tefocusDTS, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
+ggplot(tefocusRR[-1,], aes(x=tefocusGY, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
+ggplot(tefocusRR[-1,], aes(x=tefocusGYraw, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
 
-sk=read.table('../imputation/SampleToKeep.txt')
-h=read.csv('../phenotypes/BLUEs.csv')
-h$ID=str_split_fixed(h$genotype, '/', 2)[,1] ## pull off the PHZ51 tester
-hp=readRDS('../phenotypes/NAM_H-pheno.rds')
-hp$genotype=rownames(hp)
-hp$ID=str_split_fixed(rownames(hp), '/', 2)[,1] ## pull off the PHZ51 tester
-## guillaume did two grain yield blues - one adjusted by flowering time (I'm calling GY), and another with the raw values (I'm calling GYraw)
-h$GYraw=hp$GY[match(h$ID, hp$ID)] ## 
-
-a=readRDS('../imputation/matrixList_bpEachRefRange.RDS')
-tempMat=a[[1]]
-tempMat=data.frame(tempMat[rownames(tempMat) %in% sk$V1,])
-tempMat$namRIL=substr(rownames(tempMat),1,9)
-
-newMat=merge(h[,-c(1)], tempMat, by.x='ID', by.y='namRIL')
-             
-## oh dang i've got a lot of missing data... mean impute (should fix to mean of two parents!)
-for(i in 6:ncol(newMat)){
-  newMat[is.na(newMat[,i]), i] <- mean(newMat[,i], na.rm = TRUE)
-}
-
-r=sample(1:nrow(newMat), nrow(newMat)*0.8)
-
-train=newMat[r,-c(3:5)] %>% na.omit()
-dim(train)
-test=newMat[-r,-c(3:5)] %>% na.omit()
-dts=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
-#cor.test(dts[,2], dts[,3])
-
-## second 
-train=newMat[r,-c(2:3,5)] %>% na.omit()
-dim(train)
-test=newMat[-r,-c(2:3,5)] %>% na.omit()
-gy=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
-#cor.test(dts[,2], dts[,3])
-
-## third
-train=newMat[r,-c(2:4)] %>% na.omit()
-dim(train)
-test=newMat[-r,-c(2:4)] %>% na.omit()
-gyraw=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=T)
-#cor.test(dts[,2], dts[,3])
+ggplot(tefocusRR[-1,], aes(x=tefocusDTS*totalbp, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
+ggplot(tefocusRR[-1,], aes(x=tefocusGY*totalbp, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
+ggplot(tefocusRR[-1,], aes(x=tefocusGYraw*totalbp, y=totalbp, label=term, color=Classification)) + geom_text() +scale_y_log10()
 
 
-## just run fit function from above because i want the model to get coefficients!!!
-
-library(broom)
-
-rr=read.table('../imputation/maize1_reference_ranges.csv', header=T, sep=',')
-head(rr)
-rr$genic=ifelse(rr$name=='FocusRegion', T, F)
-
-
-b=broom::tidy(dts)
-
-rr$dts=b$estimate[match(paste0('X', rr$ref_range_id), b$term)]
-b=broom::tidy(gy)
-
-rr$gy=b$estimate[match(paste0('X', rr$ref_range_id), b$term)]
-b=broom::tidy(gyraw)
-
-rr$gyraw=b$estimate[match(paste0('X', rr$ref_range_id), b$term)]
-
-
-pdf('~/transfer/ridgeregression_gs.pdf', 14,6)
-ggplot(rr, aes(x=range_start, y=estimate)) + geom_point() + facet_wrap(~chrom, ncol=10)
-ggplot(rr[rr$ref_range_id!=40979,], aes(x=range_start, y=estimate)) + geom_point() + facet_wrap(~chrom, ncol=10)
 dev.off()
-sum(rr$estimate>0)
-sum(rr$estimate>0, na.rm=T)
-sum(rr$estimate<0, na.rm=T)
-sum(rr$estimate==0, na.rm=T)
-sum(rr$estimate<0, na.rm=T)
-sum(rr$estimate[rr$genic]<0, na.rm=T)
-sum(rr$estimate[!rr$genic]<0, na.rm=T)
-sum(rr$estimate[!rr$genic]>0, na.rm=T)
-sum(rr$estimate[rr$genic]>0, na.rm=T)
 
 
-
+pred_accuracy=c()
 #run for each different sets of 250 RILs (10 RILs from each family)
-foreach(sample = 1:20) %dopar% {
+#foreach(sample = 1:20) %dopar% {
 #  sample_NAM_RILs <- fread(paste0(pheno_dir, '/sample_NAM_RILs/sample_NAM_RILs', sample, '.csv'))
 #  
 #  geno_NAM_all <- geno_NAM_all[geno_NAM_all$taxa %in% sample_NAM_RILs$V2, ]
@@ -265,59 +195,37 @@ foreach(sample = 1:20) %dopar% {
 #  common_colnames_NAM <- intersect(colnames(geno_NAMfounders), colnames(geno_NAM_wona))
 #  geno_NAM <- rbind(geno_NAMfounders[,..common_colnames_NAM], geno_NAM_wona[, ..common_colnames_NAM])
   
-  accuracy_trait <- c()
-  for (trait in traits){
+#  accuracy_trait <- c()
+#  for (trait in c('DTS', 'GY', 'GYraw')){
     
-    pheno_geno_282 <- merge(pheno_282[,c('taxa',trait),with = FALSE], geno_282, by = 'taxa') 
-    pheno_geno_NAM <- merge(pheno_NAM[,c('taxa',trait),with = FALSE], geno_NAM, by = 'taxa')
+#    pheno_geno_282 <- merge(pheno_282[,c('taxa',trait),with = FALSE], geno_282, by = 'taxa') 
+#    pheno_geno_NAM <- merge(pheno_NAM[,c('taxa',trait),with = FALSE], geno_NAM, by = 'taxa')
     
-    temp_df <- predictPhenotype_crosspopulation(train_set = pheno_geno_NAM, 
-                                                test_set = pheno_geno_282, 
-                                                method = 'ridge')
-    
-    write.csv(temp_df, paste0(result_dir, '/predictedvalues_exp_haplotypes_sampleNAMtaxa/', tissue, '_', trait, '_train_NAMsample',sample, '_test_282.csv'))
-    
-    r <- cor(temp_df[,2], temp_df[,3])
-    accuracy_trait <- rbind(accuracy_trait, r)
-    message('done with trait ', trait)
-    
-  }
-  rownames(accuracy_trait) <- traits
+#    temp_df <- predictPhenotype_crosspopulation(train_set = pheno_geno_NAM, 
+ #                                               test_set = pheno_geno_282, 
+ #                                               method = 'ridge')
   
-  write.csv(accuracy_trait, paste0(result_dir, '/', tissue, '_prediction_accuracy_train_NAMsample', sample, '_test_282.csv'))
   
-  message('done with sample ', sample)
-}
+  r=sample(1:nrow(tefocus), nrow(tefocus)*0.8)
 
+    train=tefocus[r,-c(2:3,5)] %>% na.omit()
+    test=tefocus[-r,-c(2:3,5)] %>% na.omit()
+    tefocusGYcor=predictPhenotype(train_set=train, test_set=test, method='ridge', returnModel=F)
 
+#    write.csv(temp_df, paste0(result_dir, '/predictedvalues_exp_haplotypes_sampleNAMtaxa/', tissue, '_', trait, '_train_NAMsample',sample, '_test_282.csv'))
+    
+    ra <- cor(tefocusGYcor[,2], tefocusGYcor[,3])
+    pred_accuracy <- rbind(pred_accuracy, ra)
+#    message('done with trait ', trait)
+    
+#  }
+#  rownames(accuracy_trait) <- traits
+  
+#  write.csv(accuracy_trait, paste0(result_dir, '/', tissue, '_prediction_accuracy_train_NAMsample', sample, '_test_282.csv'))
+  
+#  message('done with sample ', sample)
+#}
 
-
-pdf('~/transfer/rr_test.pdf')
-plot(cv$glmnet.fit, 'lambda')
-betas=as.matrix(cv$glmnet.fit$beta)
-lambdas=cv$lambda
-names(lambdas)=colnames(betas)
-library(tidyr)
-library(dplyr)
-library(ggrepel)
-as.data.frame(betas) %>% 
-tibble::rownames_to_column("variable") %>% 
-pivot_longer(-variable) %>% 
-mutate(lambda=lambdas[name]) %>% 
-ggplot(aes(x=lambda,y=value,col=variable)) + 
-geom_line() + 
-geom_label_repel(data=~subset(.x,lambda==min(lambda)),
-aes(label=variable),nudge_x=-0.5) +
-scale_x_log10()
-as.data.frame(betas) %>% 
-tibble::rownames_to_column("variable") %>% 
-pivot_longer(-variable) %>% 
-mutate(lambda=lambdas[name]) %>% 
-ggplot(aes(x=lambda,y=value,col=variable)) + 
-geom_line() + 
-geom_label_repel(data=~subset(.x,lambda==min(lambda)),
-aes(label=variable),nudge_x=-0.5)
-dev.off()
 
 
 
