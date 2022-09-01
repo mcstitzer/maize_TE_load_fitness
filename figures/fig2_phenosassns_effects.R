13ffca4cd83f8dbebd0ac9e5e4af4d60d407fd90
mcstitzer <mcstitzer@ucdavis.edu>
2022-09-01 14:24:13 -0400

    Update fig2_phenosassns_effects.R

--- figures/fig2_phenosassns_effects.R
+++ figures/fig2_phenosassns_effects.R
@@ -122,30 +122,30 @@ plot_grid(phenocorr, legend, rel_widths=c(1,0.2))
 dev.off()
 
 
-pdf(paste0('~/transfer/fig3_phenoeffects.', Sys.Date(), '.pdf'), 14,3)
-
-## or effects as separate figure, to emphasize a totally different thing b/c by family?
-plot_grid(dtsE, gyE, phE, ncol=3, labels='AUTO')
-
-## we probably want families labeled, at least the sig ones!
-
-pahFam$sigLabels=pahFam$nam
-pahFam$sigLabels[pahFam$pval>=0.05]=''
-
-dtsEs=ggplot(pahFam[pahFam$pheno=='DTS',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal) + theme(legend.position='NULL') + ylab('') + xlab('Effect of one Mb on DTS (days)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8, box.padding = 0.5, max.overlaps = Inf, show.legend=F, direction='y') + scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
-gyEs=ggplot(pahFam[pahFam$pheno=='GY',], aes(x=effect*1e6, y=geno, col=subpop, alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ theme(legend.position='NULL')+ ylab('') + xlab('Effect of one Mb on GY (t/ha)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
-phEs=ggplot(pahFam[pahFam$pheno=='PH',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ theme(legend.position='NULL')+ ylab('') + xlab('Effect of one Mb on PH (cm)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
-plot_grid(dtsEs, gyEs, phEs, rel_widths=c(1.25,1,1), ncol=3, labels='AUTO')
-
-legend <- get_legend(
-  # create some space to the left of the legend
-  ggplot(pahFam[pahFam$pheno=='PH',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ ylab('') + xlab('Effect of one Mb on PH (cm)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
- + theme(legend.box.margin = margin(0, 0, 0, 12)) + guides(color='none', alpha='none') + labs(size='R2')
-)
-
-plot_grid(plot_grid(dtsEs, gyEs, phEs, rel_widths=c(1.25,1,1), ncol=3, labels='AUTO'), legend, rel_widths=c(1,0.1))
-
-dev.off()      
+#pdf(paste0('~/transfer/fig3_phenoeffects.', Sys.Date(), '.pdf'), 14,3)
+#
+### or effects as separate figure, to emphasize a totally different thing b/c by family?
+#plot_grid(dtsE, gyE, phE, ncol=3, labels='AUTO')
+#
+### we probably want families labeled, at least the sig ones!
+#
+#pahFam$sigLabels=pahFam$nam
+#pahFam$sigLabels[pahFam$pval>=0.05]=''
+#
+#dtsEs=ggplot(pahFam[pahFam$pheno=='DTS',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal) + theme(legend.position='NULL') + ylab('') + xlab('Effect of one Mb on DTS (days)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8, box.padding = 0.5, max.overlaps = Inf, show.legend=F, direction='y') + scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
+#gyEs=ggplot(pahFam[pahFam$pheno=='GY',], aes(x=effect*1e6, y=geno, col=subpop, alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ theme(legend.position='NULL')+ ylab('') + xlab('Effect of one Mb on GY (t/ha)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
+#phEs=ggplot(pahFam[pahFam$pheno=='PH',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ theme(legend.position='NULL')+ ylab('') + xlab('Effect of one Mb on PH (cm)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
+#plot_grid(dtsEs, gyEs, phEs, rel_widths=c(1.25,1,1), ncol=3, labels='AUTO')
+#
+#legend <- get_legend(
+#  # create some space to the left of the legend
+#  ggplot(pahFam[pahFam$pheno=='PH',], aes(x=effect*1e6, y=geno, col=subpop,  alpha=ifelse(pval<0.05, 0.9, 0.3), size=r2)) + geom_point() + geom_vline(xintercept=0, color='gray', lty='dashed')+ scale_color_manual(values=nampal)+ ylab('') + xlab('Effect of one Mb on PH (cm)')+geom_label_repel(aes(label=sigLabels), size=3, alpha=0.8,box.padding = 0.5, max.overlaps = Inf,  show.legend=F, direction='y') + theme(axis.text.y=element_blank())+ scale_size_continuous(limits=c(min(pahFam$r2), max(pahFam$r2)))
+# + theme(legend.box.margin = margin(0, 0, 0, 12)) + guides(color='none', alpha='none') + labs(size='R2')
+#)
+#
+#plot_grid(plot_grid(dtsEs, gyEs, phEs, rel_widths=c(1.25,1,1), ncol=3, labels='AUTO'), legend, rel_widths=c(1,0.1))
+#
+#dev.off()      
 
 
 
