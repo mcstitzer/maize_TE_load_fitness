ac600c6618882adca4d398aeb66486a87e94dd2f
mcstitzer <mcstitzer@ucdavis.edu>
2023-03-08 11:44:43 -0800

    Update count_teFeatures_by_haplotype.R

--- imputation/count_teFeatures_by_haplotype.R
+++ imputation/count_teFeatures_by_haplotype.R
@@ -33,62 +33,15 @@ a$refrange=b$refrange[match(a$hapid, b$hapid)]
 
 
 ######################################################
-## Read in TE annotation to count TEs per haplotype ##
+## Read in TE annotation to sum up deciles of feats ##
 ######################################################
 
-## make a different data frame for each of the NAM genomes, assign each TE in each genome into a haplotype
 
 ### do this across all NAM
 genomes=c('B73', 'CML103', 'CML247', 'CML52', 'CML69', 'Ky21', 'M162W', 'Mo18W', 'Oh43', 'P39', 
           'B97', 'CML228', 'CML277', 'CML322', 'CML333', 'HP301', 'Il14H', ## note here the capitalization of Il14H!!!!! phg uses IL14H
          'Ki11', 'Ki3', 'M37W', 'Ms71','NC350', 'NC358', 'Oh7B', 'Tx303', 'Tzi8')
 
-                
-#### add TE families!
-
-############ okay, so get the biggest families across all genotypes
-## read in all gff's so that I can find biggest by bp across all genotypes
-
-                
-## read in all TE annotations, sum by bp, then ask which contribute most bp to entire NAM
-allTE=lapply(genomes, function(genome){
-           ## read in EDTA TE annotation
- ### filenames have different capitalization than before :( so sad
- filegenome=genome
- if(genome == 'Oh7B'){filegenome='Oh7b'}
- edtafilename=list.files(path='../genomes_and_annotations/tes/new_edta_tes/', pattern=paste0(filegenome, '.+', 'split.gff3.gz'), full.names=T)
- te=import.gff(edtafilename)
- ## the new ones go B73_chr1
- seqlevels(te)=str_split_fixed(seqlevels(te), '_', 2)[,2]
- return(te)
-          })
-
-at=do.call(c, allTE)
-## need to remove _INT and _LTR from the end
-at$Name=gsub('_LTR', '', gsub('_INT', '', at$Name))
-mbTEs=data.frame(at) %>% group_by(Name, Classification) %>% dplyr::summarize(bp=sum(width)) %>% arrange(desc(bp)) %>% data.frame()
-
-write.table(mbTEs, paste0('TE_content_across_NAM_genotypes_by_fam.', Sys.Date(), '.txt'), row.names=F, col.names=T, sep='\t', quote=F)
-             
-                
-## sups too
- te=at
- te$sup=str_split_fixed(te$Classification, '/', 2)[,2]
- te$sup[te$sup=='Gypsy']='RLG'
- te$sup[te$sup=='Copia']='RLC'
- te$sup[te$sup=='unknown' & te$Classification=='LTR/unknown']='RLX'
- te$sup[te$sup=='Helitron']='DHH'
- te$sup[te$sup=='CRM']='RLG' ## need these and following ones because not structural
- te$sup[te$sup=='L1']='RIL'
- te$sup[te$sup=='RTE']='RIT'
- te$sup[te$sup=='unknown' & te$Classification=='LINE/unknown']='RIX'
- 
- te$sup[!te$Classification %in% classificationTE]=NA
-supTEs=data.frame(te[te$te,]) %>% group_by(sup) %>% dplyr::summarize(bp=sum(width)) %>% data.frame()
-write.table(supTEs, 'TE_content_across_NAM_genotypes_by_sup.txt', row.names=F, col.names=T, sep='\t', quote=F)
-
-
-              
 ## not all things in the TE file are TEs, so set up these categories
 classificationTE=c('DNA/DTA', 'DNA/DTC', 'DNA/DTH', 'DNA/DTM', 'DNA/DTT', 'DNA/Helitron', 'LINE/L1', 'LINE/RTE', 'LINE/unknown', 'LTR/CRM', 'LTR/Copia', 'LTR/Gypsy', 'LTR/unknown', 'MITE/DTA', 'MITE/DTC', 'MITE/DTH', 'MITE/DTM', 'MITE/DTT')
 classificationKnob=c('knob/TR-1', 'knob/knob180')
@@ -103,6 +56,93 @@ tesup=c("DHH", "DTA", "DTC", "DTH", "DTM", "DTT", "RIL", "RIT", "RIX", "RLC", "R
 specificRepeats=c('knob/knob180', 'knob/TR-1', 'LTR/CRM')
 names(specificRepeats)=c('knob180', 'tr1', 'crm') ## keep separate knobs, centromeric retrotransposon annotation
 
+                
+
+############ okay, so get the biggest families across all genotypes
+## read in all gff's so that I can find biggest by bp across all genotypes
+fgs=data.frame(fread('../genomes_and_annotations/genes/pan_gene_matrix_v3_cyverse.csv'))
+
+                
+## read in all TE annotations, sum by bp, then ask which contribute most bp to entire NAM
+allTE=lapply(genomes, function(genome){
+           ## read in EDTA TE annotation
+ ### filenames have different capitalization than before :( so sad
+ filegenome=genome
+ if(genome == 'Oh7B'){filegenome='Oh7b'}
+ edtafilename=list.files(path='../genomes_and_annotations/tes/new_edta_tes/', pattern=paste0(filegenome, '.+', 'split.gff3.gz'), full.names=T)
+ te=import.gff(edtafilename)
+ ## the new ones go B73_chr1
+ seqlevels(te)=str_split_fixed(seqlevels(te), '_', 2)[,2]
+          
+## read in gene annotation and give each TE a distance to gene
+          if(genome=='B73'){
+    gene=import.gff('../genomes_and_annotations/genes/NAM_gene_gffs/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.gff3')
+          }else{
+                    gene=import.gff(Sys.glob(paste0('../genomes_and_annotations/genes/NAM_gene_gffs/Zm-', genome, '-REFERENCE-NAM-1.0_*.gff3')))
+                    }
+   fgs.genome=fgs[,genome][fgs$class=='Core Gene' | fgs$class=='Near-Core Gene']
+  nontandemcore=gene[gene$canonical_transcript=='1' & !is.na(gene$canonical_transcript) & gene$ID %in% fgs.genome & !is.na(gene$ID),]
+  ## there are core genes without a model called, so add these
+  coreregions=GRanges(seqnames=str_split_fixed(gsub('gmap_ID=', '',fgs.genome[grepl('gmap', fgs.genome)]), ':', 2)[,1], 
+                      IRanges(start=as.numeric(str_split_fixed(str_split_fixed(gsub('gmap_ID=', '',fgs.genome[grepl('gmap', fgs.genome)]), ':', 2)[,2], "-", 2)[,1]), 
+                              end=as.numeric(str_split_fixed(str_split_fixed(gsub('gmap_ID=', '',fgs.genome[grepl('gmap', fgs.genome)]), ':', 2)[,2], "-", 2)[,2])))
+
+            te$genedist=NA
+  te$genedist[queryHits(distanceToNearest(te, nontandemcore))]=mcols(distanceToNearest(te, nontandemcore, ignore.strand=T))$distance
+  te$coredist=NA
+  te$coredist[queryHits(distanceToNearest(te, c(nontandemcore,coreregions)))]=mcols(distanceToNearest(te, c(nontandemcore, coreregions), ignore.strand=T))$distance
+
+### read in umr counts for each TE - these should be same dimensions as te annotation (te)
+          umr=read.table(paste0('../te_summaries/',genome, '-TE-UMR.bed'))
+          te$umrCount=umr$V10
+          te$umrBp=umr$V11
+          te$umrCoverage=umr$V13
+          
+ return(te)
+          })
+
+at=do.call(c, allTE)
+## need to remove _INT and _LTR from the end
+at$Name=gsub('_LTR', '', gsub('_INT', '', at$Name))
+mbTEs=data.frame(at) %>% group_by(Name, Classification) %>% dplyr::summarize(bp=sum(width)) %>% arrange(desc(bp)) %>% data.frame()
+
+#write.table(mbTEs, paste0('TE_content_across_NAM_genotypes_by_fam.', Sys.Date(), '.txt'), row.names=F, col.names=T, sep='\t', quote=F)
+       
+                
+                
+                
+## sups too
+ te=at
+ te$sup=str_split_fixed(te$Classification, '/', 2)[,2]
+ te$sup[te$sup=='Gypsy']='RLG'
+ te$sup[te$sup=='Copia']='RLC'
+ te$sup[te$sup=='unknown' & te$Classification=='LTR/unknown']='RLX'
+ te$sup[te$sup=='Helitron']='DHH'
+ te$sup[te$sup=='CRM']='RLG' ## need these and following ones because not structural
+ te$sup[te$sup=='L1']='RIL'
+ te$sup[te$sup=='RTE']='RIT'
+ te$sup[te$sup=='unknown' & te$Classification=='LINE/unknown']='RIX'
+ 
+ te$sup[!te$Classification %in% classificationTE]=NA
+                
+                
+##define bins
+telengthbins=quantile(width(te[te$sup %in% tesup]), probs = seq(0, 1, by = .1))
+genedistbins=quantile(te[te$sup %in% tesup,]$genedist, probs = seq(0, 1, by = .1))
+coredistbins=quantile(te[te$sup %in% tesup,]$coredist, probs = seq(0, 1, by = .1))
+agebins=quantile(as.numeric(te[te$sup %in% tesup,]$Identity), probs = seq(0, 1, by = .1), na.rm=T)
+umrbins= 
+
+                
+                
+                
+                
+                
+supTEs=data.frame(te[te$te,]) %>% group_by(sup) %>% dplyr::summarize(bp=sum(width)) %>% data.frame()
+#write.table(supTEs, 'TE_content_across_NAM_genotypes_by_sup.txt', row.names=F, col.names=T, sep='\t', quote=F)
+
+
+              
                
 ### NOW, check whether TE is within the quantile or decile of length, genedist, umr, age
 ### THIS IS WHAT YOU NEED TO DO!!!!!
@@ -124,6 +164,7 @@ names(tes)=genomes
 #######################################################3
 ###### OKAY, there's an updated version of EDT annotations for NAM
 
+                
 
 ## loop through and populate the entry of tes
 for(genome in genomes){
@@ -171,7 +212,13 @@ for(genome in genomes){
 ## get the hapids that come from this genome
  haps=GRanges(seqnames=paste0('chr', a$chr[a$genotype==genome]), IRanges(start=a$startmin[a$genotype==genome], end=a$endmax[a$genotype==genome]))
  haps$hapid=a$hapid[a$genotype==genome]
-
+ 
+          
+          ### get count of age bins
+          ### get count of gene dist bins
+          ### get count of UMR bins
+          ### get count of TE length
+          
 
   ### by "family"
   for(fam in mbTEs$Name[mbTEs$bp>10000000& !mbTEs$Classification %in% unlist(otherClassifications)]){
