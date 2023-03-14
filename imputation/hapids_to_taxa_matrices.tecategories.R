79045ceb13118766a09bc2f73629b51648eda665
mcstitzer <mcstitzer@ucdavis.edu>
2023-03-14 16:45:09 -0400

    Update hapids_to_taxa_matrices.tecategories.R

--- imputation/hapids_to_taxa_matrices.tecategories.R
+++ imputation/hapids_to_taxa_matrices.tecategories.R
@@ -23,6 +23,11 @@ atorrm=read.table('coregene_distance_bins.txt', header=T, comment.char='')
 atorrm=dcast(atorrm, hapid~genecat)
 atorrm[is.na(atorrm)]=0 ## ncie and this mkes sense because ranges alternate between genic intergenic and 0 and value ingene
 
+## add columns for umr
+a2=read.table('umr_bins.txt', header=T, comment.char='')
+a2=dcast(a2, hapid~umrCountnonzero)
+a2[is.na(a2)]=0 ## ncie and this mkes sense because ranges alternate between genic intergenic and 0 and value ingene
+colnames(a2)[2:3]=c('noUMR', 'hasUMR')
 
 ## this part takes a long long time (2 hours??) - so it's good i've pulled it out from the below lapply :~)
 matchMatrix=data.frame(lapply(1:ncol(all.haps), function(refrange) match(all.haps[,refrange], atorrm$hapid)))
@@ -30,7 +35,7 @@ colnames(matchMatrix)=colnames(all.haps)
 
 ## okay leaving for a run
 
-
+atorrm=merge(atorrm, a2)
                               
 ## get rid of b73 flavors and bad ranges
 b=read.table('refranges_B73correctlygenotypedAND1Mbrangeremoved.2022-03-22.txt', header=T)
