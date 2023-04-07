0f96d6bf3eb3c0ce0a7e51260300b8213a36da2a
mcstitzer <mcstitzer@ucdavis.edu>
2023-04-07 11:20:07 -0700

    Update run_EDTA_phz51.sh

--- genomes_and_annotations/run_EDTA_phz51.sh
+++ genomes_and_annotations/run_EDTA_phz51.sh
@@ -8,3 +8,4 @@ singularity exec --no-home ./EDTA.sif EDTA.pl --genome 5_Buckler-PHZ51_mecatErro
 ## or dang, why not just repeatmask with the NAM library - that's got to be closer...
 export PATH=/programs/RepeatMasker_4-1-0:$PATH
 RepeatMasker -e ncbi -pa 24 -q -no_is -norna -nolow -div 40 -lib NAM.EDTA2.0.0.MTEC02052020.TElib.fa -cutoff 225 5_Buckler-PHZ51_mecatErrorCorrected.contigs.fasta
+/programs/RepeatMasker_4-1-0/util/rmOutToGFF3.pl 5_Buckler-PHZ51_mecatErrorCorrected.contigs.fasta.out > 5_Buckler-PHZ51_mecatErrorCorrected.contigs.RepeatMasker.gff3
