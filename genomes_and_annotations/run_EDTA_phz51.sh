c9e9d8008d2868188d336d2a874518c8c9d25e0b
mcstitzer <mcstitzer@ucdavis.edu>
2023-04-20 13:51:37 -0700

    Update run_EDTA_phz51.sh

--- genomes_and_annotations/run_EDTA_phz51.sh
+++ genomes_and_annotations/run_EDTA_phz51.sh
@@ -1,8 +1,8 @@
 
-singularity pull EDTA.sif docker://oushujun/edta:2.0.0
-
-export LC_ALL=C
-singularity exec --no-home ./EDTA.sif EDTA.pl --genome 5_Buckler-PHZ51_mecatErrorCorrected.contigs.fasta --cds genome.cds.fa --curatedlib NAM.EDTA2.0.0.MTEC02052020.TElib.fa --overwrite 1 --sensitive 1 --anno 1 --evaluate 1 --threads 10
+#singularity pull EDTA.sif docker://oushujun/edta:2.0.0
+#
+#export LC_ALL=C
+#singularity exec --no-home ./EDTA.sif EDTA.pl --genome 5_Buckler-PHZ51_mecatErrorCorrected.contigs.fasta --cds genome.cds.fa --curatedlib NAM.EDTA2.0.0.MTEC02052020.TElib.fa --overwrite 1 --sensitive 1 --anno 1 --evaluate 1 --threads 10
 
 
 ## or dang, why not just repeatmask with the NAM library - that's got to be closer...
