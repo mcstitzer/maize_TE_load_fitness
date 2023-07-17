070371d1c011b363053b1bd600f9dddf4686584d
mcstitzer <mcstitzer@ucdavis.edu>
2023-07-17 11:12:42 -0700

    Update hapids_to_taxa_matrices.tecategories.R

--- imputation/hapids_to_taxa_matrices.tecategories.R
+++ imputation/hapids_to_taxa_matrices.tecategories.R
@@ -36,10 +36,11 @@ a3[is.na(a3)]=0 ## ncie and this mkes sense because ranges alternate between gen
 colnames(a3)[2:3]=c('olderInsertion', 'recentInsertion')
 
 ## add columns for deleterious disasters
-a4=read.table('youngclose_bins.txt', header=T, comment.char='')
-a4=dcast(a4, hapid~young)
+a4=read.table('youngclose_bins.2023-07-14.txt', header=T, comment.char='')
+a4=dcast(a4, hapid~youngand1kb)
 a4[is.na(a4)]=0 
-colnames(a4)[2:3]=c('youngandclose', 'farorold')
+colnames(a4)[2:3]=c( 'farorold','youngandclose')
+
 
 
 ## this part takes a long long time (2 hours??) - so it's good i've pulled it out from the below lapply :~)
@@ -50,7 +51,8 @@ colnames(matchMatrix)=colnames(all.haps)
 
 atorrm=merge(atorrm, a2)
 atorrm=merge(atorrm, a3)
-                              
+atorrm=merge(atorrm, a4)
+                             
 ## get rid of b73 flavors and bad ranges
 b=read.table('refranges_B73correctlygenotypedAND1Mbrangeremoved.2022-03-22.txt', header=T)
 
@@ -70,3 +72,31 @@ for(tefam in colnames(atorrm)[2:ncol(atorrm)]){
                     
 
 write.table(tefams, paste0('ril_tecategories_bp.', Sys.Date(), '.txt'), row.names=F, col.names=T, quote=F, sep='\t')
+
+
+##### actually do for te families
+
+
+b=read.table('refranges_B73correctlygenotypedAND1Mbrangeremoved.2022-03-22.txt', header=T)
+keepColnames=colnames(all.haps)%in% paste0('X',b$refrange[b$KEEPfinalFilter])
+          
+## add columns for deleterious disasters
+a5=read.table('TEfams_load_bins.2023-07-14.txt', header=T, comment.char='')
+a5=dcast(a5, hapid~collapsedFam)
+a5[is.na(a5)]=0 
+       
+                            
+tefams=data.frame(RIL=rownames(all.haps))
+
+for(tefam in colnames(a5)[2:ncol(a5)]){
+ tempMat=sapply(1:ncol(all.haps), function(refrange) a5[matchMatrix[,refrange],tefam])
+ rownames(tempMat)=rownames(all.haps)
+ colnames(tempMat)=colnames(all.haps)
+ tefams[,tefam]=rowSums(tempMat[,keepColnames], na.rm=T)
+}
+
+                    
+
+write.table(tefams, paste0('ril_tefamilycounts_1MbFams_bp.', Sys.Date(), '.txt'), row.names=F, col.names=T, quote=F, sep='\t')
+
+                
