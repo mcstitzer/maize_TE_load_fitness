5ce9e8d4d4b8e186734d31eaf5a16ff504b01799
mcstitzer <mcstitzer@ucdavis.edu>
2023-08-07 11:50:53 -0700

    Create map_to_genes.sh

---
+++ imputation/map_282/map_to_genes.sh
@@ -0,0 +1,13 @@
+
+
+# wget https://download.maizegdb.org/Zm-B73-REFERENCE-NAM-5.0/Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.cds.fa.gz
+
+function map_fq(){  minimap2 -ax sr -t 2 Zm-B73-REFERENCE-NAM-5.0_Zm00001eb.1.cds.fa ${1}_1.clean.fq.gz ${1}_2.clean.fq.gz | samtools view -Sb - | samtools sort - -O bam -o ${1}.B73CDS.bam ; }
+
+export -f map_fq
+cat 282_fqnames.txt | parallel --progress --jobs 31 map_fq {}
+
+
+for i in *B73CDS.bam; do samtools index $i & done
+
+for i in *B73CDS.bam; do samtools stats $i > ${i%.*}.B73genemapped.txt & done
